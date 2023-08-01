eb86a1766052a8f935648d7cfe49ebce48dee220
hugh2k1 <30531408+hugh2k1@users.noreply.github.com>
2023-08-01 13:04:48 +0000

    CoPilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create a web server
+// Start the web server
+// Listen for requests
+// Respond to requests
+// Read the request body
+// Parse the request body
+// Store the comment
+// Send a response
+// Read the comments
+// Send the comments
+// Respond to the request
+// Send the comments
+// Send
